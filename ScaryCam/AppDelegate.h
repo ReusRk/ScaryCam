6d2058d13f7765ac18f722d06fbb1cf7c0944255
Fluxtech MacMini1 <fluxtech_macmini1@fluxtech-macmini1s-mac-mini.local>
2016-07-01 11:18:39 +0530

    Initial Commit

---
+++ ScaryCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScaryCam
+//
+//  Created by Fluxtech_MacMini1 on 7/1/16.
+//  Copyright © 2016 Fluxtech_MacMini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScaryCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScaryCam
+//
+//  Created by Fluxtech_MacMini1 on 7/1/16.
+//  Copyright © 2016 Fluxtech_MacMini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
